d0b08f583142f1f0a3aadf95356b7e1348a723ef
brutalhonesty <sparky1010@gmail.com>
2013-12-17 10:25:06 -0700

    Added testes and a more robust image method

--- index.js
+++ index.js
@@ -25,10 +25,24 @@ api.board = function(board) {
 	var subapi = {};
 	subapi._board = board;
 	
-	subapi.image = function(file) {
+	subapi.image = function(name, ext) {
+		name = parseInt(name, 10);
+		if(typeof(name) !== 'number' || isNaN(name)) {
+			return null;
+		} else if(typeof(ext) !== 'string') {
+			return null;
+		}
+		var file = name + ext;
 		return ["http://i.4cdn.org", board, "src", file].join("/");
 	};
-	subapi.imageCache = function(file) {
+	subapi.imageCache = function(name, ext) {
+		name = parseInt(name, 10);
+		if(typeof(name) !== 'number' || isNaN(name)) {
+			return null;
+		} else if(typeof(ext) !== 'string') {
+			return null;
+		}
+		var file = name + ext;
 		return ["http://i.4chanlink.org", board, "src", file].join("/");
 	};
 
--- test/main.js
+++ test/main.js
@@ -9,11 +9,11 @@ describe('4chan api', function() {
     it('should return a list of boards', function(done) {
       this.timeout(5000);
       api.boards(function(err, boards){
-      	should.not.exist(err);
-      	should.exist(boards);
-      	Array.isArray(boards).should.equal(true);
-      	boards.length.should.not.equal(0);
-      	done();
+        should.not.exist(err);
+        should.exist(boards);
+        Array.isArray(boards).should.equal(true);
+        boards.length.should.not.equal(0);
+        done();
       });
     });
   });
@@ -32,13 +32,13 @@ describe('4chan api', function() {
       this.timeout(5000);
       var board = api.board('b');
       board.catalog(function(err, pages){
-      	should.not.exist(err);
-      	should.exist(pages);
-      	Array.isArray(pages).should.equal(true);
-      	pages.length.should.not.equal(0);
-      	should.exist(pages[0].page);
-      	pages[0].page.should.equal(0);
-      	done();
+        should.not.exist(err);
+        should.exist(pages);
+        Array.isArray(pages).should.equal(true);
+        pages.length.should.not.equal(0);
+        should.exist(pages[0].page);
+        pages[0].page.should.equal(0);
+        done();
       });
     });
   });
@@ -48,12 +48,12 @@ describe('4chan api', function() {
       this.timeout(5000);
       var board = api.board('b');
       board.page(0, function(err, threads){
-      	should.not.exist(err);
-      	should.exist(threads);
-      	Array.isArray(threads).should.equal(true);
-      	threads.length.should.not.equal(0);
-      	should.exist(threads[0].posts);
-      	done();
+        should.not.exist(err);
+        should.exist(threads);
+        Array.isArray(threads).should.equal(true);
+        threads.length.should.not.equal(0);
+        should.exist(threads[0].posts);
+        done();
       });
     });
   });
@@ -63,12 +63,12 @@ describe('4chan api', function() {
       this.timeout(5000);
       var board = api.board('b');
       board.threads(function(err, pages){
-      	should.not.exist(err);
-      	should.exist(pages);
-      	Array.isArray(pages).should.equal(true);
-      	pages.length.should.not.equal(0);
-      	should.exist(pages[0].threads);
-      	done();
+        should.not.exist(err);
+        should.exist(pages);
+        Array.isArray(pages).should.equal(true);
+        pages.length.should.not.equal(0);
+        should.exist(pages[0].threads);
+        done();
       });
     });
   });
@@ -78,15 +78,68 @@ describe('4chan api', function() {
       this.timeout(5000);
       var board = api.board('b');
       board.threads(function(err, pages){
-      	board.thread(pages[0].threads[0].no, function(err, thread){
-      		should.not.exist(err);
-	      	should.exist(thread);
-	      	Array.isArray(thread).should.equal(true);
-	      	thread.length.should.not.equal(0);
-      		done();
-      	})
+        board.thread(pages[0].threads[0].no, function(err, thread){
+          should.not.exist(err);
+          should.exist(thread);
+          Array.isArray(thread).should.equal(true);
+          thread.length.should.not.equal(0);
+          done();
+        })
       });
     });
   });
 
+  describe('board.image()', function () {
+    this.timeout(5000);
+    var board = api.board('b');
+    it('should return a string of the image', function(done) {
+      var image1 = board.image(1387116137513, '.jpg');
+      should.exist(image1);
+      image1.should.equal('http://i.4cdn.org/b/src/1387116137513.jpg');
+      var image2 = board.image('1387116137513', '.jpg');
+      should.exist(image2);
+      image2.should.equal('http://i.4cdn.org/b/src/1387116137513.jpg');
+      done();
+    });
+    it('should handle NaN\'s and nulls', function(done) {
+      var image3 = board.image(NaN, '.jpg');
+      should.strictEqual(null, image3);
+      var image4 = board.image(null, '.jpg');
+      should.strictEqual(null, image4);
+      var image5 = board.image(null, null);
+      should.strictEqual(null, image5);
+      var image6 = board.image(1387116137513, null);
+      should.strictEqual(null, image6);
+      var image7 = board.image('1387116137513', null);
+      should.strictEqual(null, image6);
+      done();
+    });
+  });
+
+  describe('board.imageCache()', function () {
+    this.timeout(5000);
+    var board = api.board('b');
+    it('should return a string of the image', function(done) {
+      var image1 = board.imageCache(1387116137513, '.jpg');
+      should.exist(image1);
+      image1.should.equal('http://i.4chanlink.org/b/src/1387116137513.jpg');
+      var image2 = board.imageCache('1387116137513', '.jpg');
+      should.exist(image2);
+      image2.should.equal('http://i.4chanlink.org/b/src/1387116137513.jpg');
+      done();
+    });
+    it('should handle NaN\'s and nulls', function(done) {
+      var image3 = board.imageCache(NaN, '.jpg');
+      should.strictEqual(null, image3);
+      var image4 = board.imageCache(null, '.jpg');
+      should.strictEqual(null, image4);
+      var image5 = board.imageCache(null, null);
+      should.strictEqual(null, image5);
+      var image6 = board.imageCache(1387116137513, null);
+      should.strictEqual(null, image6);
+      var image7 = board.imageCache('1387116137513', null);
+      should.strictEqual(null, image6);
+      done();
+    });
+  });
 });
